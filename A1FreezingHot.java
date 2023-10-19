2a1e78e008f29e410dd19f5d6838d5a75340855f
Gordon <gordon.zhu25@ycdsbk>
2023-10-19 09:31:30 -0400

    Modified Code

--- A1FreezingHot.java
+++ A1FreezingHot.java
@@ -1,1 +1,17 @@
+class A1FreezingHot extends ConsoleProgram {
+
+  /**
+  * Given two temperatures via user input, output true if one is less than 0 and the other is greater than 100.
+  * @author: Gordon Z
+  */
+  
+  public void run() {
+
+    int a = readInt("first temperature: ");
+    int b = readInt("first temperature: ");
+
+    println((a<0&&b>100)||(b<0&&a>100));
+    
+  }
+}
 
